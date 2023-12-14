f53e7a693ce15d88f3fa4db610a50e0f38122e04
Ryan <ryan_walker184@hotmail.com>
2023-12-13 23:12:22 -0500

    Second Commit - Spelling corrections to html and updates to JS variables.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,42 @@
 // Set Variables
-var searchBarEl = document.querySelector('.searchInput');
 var recipeListEl = document.querySelector('.recipeList');
 var recipeEl = [];
+// Spoonacular API Variables
+var baseUrl = 'https://api.spoonacular.com/';
+var apiKey = 'ef9c25b2e22b4e0d8363c1109951f721';
+// Recipe by ingredients Variables
+var ingApi = 'recipes/findByIngredients?';
+var ingInput = document.querySelector('.searchInput');
+// Random Recipe Variables
+var randomUrl = `${baseUrl}recipes/random?`;
 
-
+// Function to create list of recipes from the user input.
 $(function getIngList() {
-    var listUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=ef9c25b2e22b4e0d8363c1109951f721&ingredients=' + searchBarEl.value + '&number=5&ranking=2&ignorePantry=true'
+    var listUrl = `${baseUrl}${ingAPI}apiKey=${apiKey}&ingredients=${ingInput.values}&number=5&ranking=2&ignorePantry=true`;
 
     fetch(listUrl)
         .then(function (response) {
             return response.json();
-        })
-        .then(function (data) {
+        }) 
+        // create elements/containers for recipe elements
+        .then(function (data) { 
             for (var i = 0; i < listUrl.length; i++) {
                 var recipeEl = document.createElement('section');
                 recipeEl[i].setAttribute('class', 'row recipe' );
                 var imgEl = document.createElement('section');
                 imgEl.setAttribute('class', 'col-3 recipeImg');
                 var infoEl = document.createElement('section');
-                infoEl.setAttribute('class', 'col-9 description')
+                infoEl.setAttribute('class', 'col-9 infoBox')
                 var recipeNameEl = document.createElement('h2');
                 recipeNameEl.setAttribute('class', 'row recipeName');
                 var descriptionEl = document.createElement('p');
                 descriptionEl.setAttribute('class', 'row description');
-
-
+                // Append Elements
                 recipeListEl.append(recipeEl);
                 recipeEl.append(imgEl);
                 recipeEl.append(infoEl);
                 infoEl.append(recipeNameEl);
                 infoEl.append(descriptionEl);
-                )
-
-                
-
-
-
             }
-        })
-
-
-
-
-})
-
-$()
+        });
+    })
