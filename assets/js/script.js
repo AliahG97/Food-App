6df965130ad0484c42b6bb7d2a402f2b6debee5e
Ryan <ryan_walker184@hotmail.com>
2023-12-14 21:32:23 -0500

    Updated JS to working copy of get recipe by ingredient

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,90 @@
 // Set Variables
 var searchBarEl = document.querySelector('.searchInput');
 var recipeListEl = document.querySelector('.recipeList');
+var recipeSection = document.querySelector('.recipeSection')
 var recipeEl = [];
+var searchBtnEl = document.querySelector('.submitBtn');
+//Spoonacular API Variables
+var baseUrl = 'https://api.spoonacular.com/';
+var byIngredients = 'recipes/findByIngredients?';
+var summarizeRecipe;
+var apiKey = 'ef9c25b2e22b4e0d8363c1109951f721';
+var userIngredients;
+var byIngredientsUrl;
+var summarizeRecipeUrl;
+var ignorePantry = 'true';
+var ranking = '2';
+var recipeNum = '5';
+var recipes;
+var images;
+var description;
+var recipeName;
 
-
-$(function getIngList() {
-    var listUrl = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=ef9c25b2e22b4e0d8363c1109951f721&ingredients=' + searchBarEl.value + '&number=5&ranking=2&ignorePantry=true'
-
-    fetch(listUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            for (var i = 0; i < listUrl.length; i++) {
+function createRecipeList() {
+    for (var i = 0; i < recipes.length; i++) {
+        summarizeRecipeUrl = `${baseUrl}recipes/${recipes[i].id}/summary?apiKey=${apiKey}`
+        fetch(summarizeRecipeUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                } else {
+                    alert(`Error: ${response.statusText}`);
+                }
+            })
+            .then(function (data) {
+                console.log(data);
+                description = data;
+            }).then(function () {
                 var recipeEl = document.createElement('section');
-                recipeEl[i].setAttribute('class', 'row recipe' );
-                var imgEl = document.createElement('section');
+                recipeEl.setAttribute('class', 'row recipe');
+                var imgEl = document.createElement('img');
                 imgEl.setAttribute('class', 'col-3 recipeImg');
+                imgEl.setAttribute('src', `${recipes[i].image}`);
                 var infoEl = document.createElement('section');
-                infoEl.setAttribute('class', 'col-9 description')
+                infoEl.setAttribute('class', 'col-9 description');
                 var recipeNameEl = document.createElement('h2');
                 recipeNameEl.setAttribute('class', 'row recipeName');
+                recipeNameEl.textContent = recipes[i].title;
                 var descriptionEl = document.createElement('p');
                 descriptionEl.setAttribute('class', 'row description');
 
-
-                recipeListEl.append(recipeEl);
+                // Append recipe results to webpage.
+                recipeSection.append(recipeEl);
                 recipeEl.append(imgEl);
                 recipeEl.append(infoEl);
                 infoEl.append(recipeNameEl);
                 infoEl.append(descriptionEl);
-                )
+            })
+    }};
 
-                
+    function getRecipeList() {
+        byIngredientsUrl = `${baseUrl}${byIngredients}apiKey=${apiKey}&ingredients=${userIngredients}&number=${recipeNum}&ranking=${ranking}&ingorePantry=${ignorePantry}`;
 
+        fetch(byIngredientsUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                } else {
+                    alert(`Error: ${response.statusText}`);
+                }
+            })
+            .then(function (data) {
+                console.log(data);
+                recipes = data;
+            })
+            .then(function (data) {
+                createRecipeList();
+            })
+    };
 
-
-            }
-        })
-
-
-
-
-})
-
-$()
+    searchBtnEl.addEventListener('click', function () {
+        userIngredients = searchBarEl.value;
+        console.log(userIngredients);
+        if (userIngredients === '') {
+            alert('Please list the ingredients you want in the recipes.');
+        } else {
+            getRecipeList();
+        }
+    });
