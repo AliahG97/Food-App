76277881252ffd26824c4bb2dac8e61385d75cbf
Ryan <ryan_walker184@hotmail.com>
2024-01-03 00:28:38 -0500

    Updates to API-1 functionality -Added links to spoonacular Recipe Pages. -Updated responsiveness of recipe boxes. -Created Code for local storage of recently clicked recipe links. -Some Styling changes in CSS to webpage elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@ var searchBtnEl = document.querySelector('.submitBtn');
 var baseUrl = 'https://api.spoonacular.com/';
 var byIngredients = 'recipes/findByIngredients?';
 var summarizeRecipe;
-var apiKey = 'e3d6ede19b8b4ac39a3e036fefe8455a';
+//var apiKey = 'e3d6ede19b8b4ac39a3e036fefe8455a';
+var apiKey = 'ef9c25b2e22b4e0d8363c1109951f721';
 
 var userIngredients;
 var byIngredientsUrl;
@@ -18,20 +19,23 @@ var recipeNum = '5';
 var images;
 var description;
 var recipeName;
+var recipeHistory = [];
 
 function createRecipeList(recipes, description) { // Create recipe elements and append to section of the page
     for (i = 0; i < recipes.length; i++) {
         var recipeEl = document.createElement('section');
         recipeEl.setAttribute('class', 'row recipe');
         var imgEl = document.createElement('img');
-        imgEl.setAttribute('class', 'col-3 recipeImg');
+        imgEl.setAttribute('class', 'col-lg-3 col-md-12 recipeImg');
         imgEl.setAttribute('src', `${recipes[i].image}`);
         var infoEl = document.createElement('section');
-        infoEl.setAttribute('class', 'col-9 description');
-        var recipeNameEl = document.createElement('h2');
+        infoEl.setAttribute('class', 'col-lg-9 col-md-12 description');
+        var recipeNameEl = document.createElement('a');
         recipeNameEl.setAttribute('class', 'row recipeName');
+        recipeNameEl.setAttribute('href', `https://spoonacular.com/recipes/${recipes[i].title}-${recipes[i].id}`); //Create clickable text that will take you to the recipe webpage.
+        recipeNameEl.setAttribute('target', '_blank');
         recipeNameEl.textContent = recipes[i].title;
-        var descriptionEl = document.createElement('p');
+        var descriptionEl = document.createElement('pre');
         descriptionEl.setAttribute('class', 'row description');
         descriptionEl.innerHTML = description[i].summary;
         // Append recipe results to webpage.
@@ -43,27 +47,33 @@ function createRecipeList(recipes, description) { // Create recipe elements and 
     }
 };
 
-function getRecipeDescription(recipes) { // Get the recipe description using the ID from the fetch in getRecipeList()
-    var recipeData = [];
-    for (i = 0; i < recipes.length; i++) {
-        console.log('id: ' + recipes[i].id);
-        var recipeDescriptionUrl = `${baseUrl}recipes/${recipes[i].id}/summary?apiKey=${apiKey}`;
-        fetch(recipeDescriptionUrl) // Call uses 1 point per call (5 recipes = 5 points)
-            .then(function (response) {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    alert(`Error: ${response.statusText}`);
-                }
-            })
-            .then(function (data) {
-                console.log(data);
-                recipeData.push(data);
-            });
-    };
-    console.log(recipeData);
-    createRecipeList(recipes, recipeData);
-};
+async function getRecipeDescription(recipes) {
+    // Map each recipe to a fetch promise
+    const fetchPromises = recipes.map(async (recipe) => {
+        const recipeDescriptionUrl = `${baseUrl}recipes/${recipe.id}/summary?apiKey=${apiKey}`;
+
+        try {
+            const response = await fetch(recipeDescriptionUrl);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json(); // Parse and return the JSON data
+        } catch (error) {
+            console.error('Error fetching recipe description:', error);
+            return null; // Return null or some error indication as per your logic
+        }
+    });
+
+    // Wait for all the fetch promises to resolve
+    const recipeData = await Promise.all(fetchPromises);
+
+    // Filter out any null results if necessary
+    const validRecipeData = recipeData.filter(data => data !== null);
+    console.log(validRecipeData);
+    createRecipeList(recipes, validRecipeData);
+}
+
+
 
 function getRecipeList() { // Get list of recipes the use the entered ingredients
     byIngredientsUrl = `${baseUrl}${byIngredients}apiKey=${apiKey}&ingredients=${userIngredients}&number=${recipeNum}&ranking=${ranking}&ingorePantry=${ignorePantry}`;
@@ -96,6 +106,71 @@ searchBtnEl.addEventListener('click', function () { // Add click listener event 
         getRecipeList();
     }
 });
+// Add Event Listener to any Anchor Element on the Page
+$(document).on('click', 'a', function () { 
+    var recipe = {
+        title: this.textContent,
+        href: this.href
+    };
+
+    storeRecentRecipes(recipe); // When a link is clicked, this will call to update the recent recipes list
+})
+
+//Store the recipe name and href from the associated anchor element in local Storage
+function storeRecentRecipes(latestRecipe) {
+    var storedRecipes = JSON.parse(localStorage.getItem('recentRecipes')) || [];
+
+    if (!Array.isArray(storedRecipes)) {
+        storedRecipes = [];
+    }
+
+    var maxStoredRecipes = 20; //Limit the amount of recent Recipes to save.
+
+    var isDuplicate = storedRecipes.some(function (recipe) {
+        return recipe.href === latestRecipe.href;
+    });
+
+    if (!isDuplicate) {
+        if (storedRecipes.length >= maxStoredRecipes) {
+            storedRecipes.shift();
+        }
+
+        storedRecipes = [...storedRecipes, latestRecipe];
+        localStorage.setItem('recentRecipes', JSON.stringify(storedRecipes));
+    }
+
+    renderRecentRecipes();
+}
+
+// Append all recent recipes to the HTML as href links.
+function renderRecentRecipes() {
+    var recipeHistory = document.getElementById('otherRecipes');
+    recipeHistory.innerHTML = 'Previously Viewed Recipes';
+    
+
+    var storedRecipes = JSON.parse(localStorage.getItem('recentRecipes')) || [];
+    //render and element for each recent search
+    for (var i = 0; i < storedRecipes.length; i++) {
+        var recentRecipeEl = document.createElement('a');
+        recentRecipeEl.setAttribute('class', 'row recentRecipe');
+        recentRecipeEl.setAttribute('data-index', i);
+        recentRecipeEl.setAttribute('href', storedRecipes[i].href);
+        recentRecipeEl.setAttribute('target', '_blank');
+        recentRecipeEl.textContent = storedRecipes[i].title;
+
+
+        recipeHistory.appendChild(recentRecipeEl);
+    }
+}
+
+function init() { //Populate the Recent Recipes from local storage on initial page load
+    var storedRecipes = JSON.parse(localStorage.getItem('recentRecipes')) || [];
+
+    if (storedRecipes !== null) {
+        recipeHistory = storedRecipes;
+    }
+    renderRecentRecipes();
+}
 
 async function getRandomRecipes() {
     // TODO: fix the url (route)
@@ -124,3 +199,5 @@ recipeListEl.addEventListener('click', function () {
     console.log('hello')
     getRandomRecipes();
 })
+
+init();
